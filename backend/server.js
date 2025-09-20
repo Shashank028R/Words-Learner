65111cf63e73d8fa2770d3833f91bf8d4aa6d9a8
Shashank028R <shashankmuz3@gmail.com>
2025-09-21 03:59:51 +0530

    newChanges

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,5 @@ app.get('/', (req, res) => {
     res.send('<h1>Learn Words Backend Running<h1>');
 });
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
